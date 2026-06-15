d86545d656919b17efee20b971529886ce2b4b53
joaohanseng <joaohanseng@gmail.com>
2026-06-15 10:33:41 -0300

    Adicionando o menu e curriculo

--- modulos/menu.php
+++ modulos/menu.php
@@ -6,6 +6,8 @@
         <a href="#home" class="tabs">Início</a>
         <a href="#recursos" class="tabs">Recursos</a>
         <a href="#contato" class="tabs">Contato</a>
+        <a href="#curriculo" class="tabs">Currículo</a>
+        <a href="portifolio" class="tabs">Portfolio</a>
     </div>
 </nav>
 <hr>
